be23375f24fa34c6677bd1e36c6d55ab405fbb63
Ramiroqr <ramiroquisper@gmail.com>
2022-03-11 18:43:08 -0500

    Finish date class

---
+++ ConceptsPOO02/ConceptsPOO02/Date.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConceptsPOO02
+{
+    public class Date
+    {
+        public int _year { get; set; }
+
+        public int _month { get; set; }
+
+        public int _day { get; set; }
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = CheckMonth(month);
+            _day = CheckDay(year, month, day);
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            if (month == 2 && day == 29 && isLeapYear(year))
+            {
+                return day;
+            }
+
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <=  daysPerMonth[month])
+            {
+                return day;
+            }
+            
+            throw new DayException("Invalid day");
+        }
+
+        private bool isLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+        }
+
+        private int CheckMonth(int month)
+        {
+            if(month >= 1 && month <= 12)
+            {
+                return month;
+            }
+
+            throw new MonthException("Invalid month");
+        }
+
+        public override string ToString()
+        {
+            return $"{_year}/{_month:00}/{_day:00}";
+        }
+    }
+}
